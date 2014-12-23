5ebd6539866f530c9deecbc63528fae5e6f09dbb
jiangyongsheng <jiangyongsheng@ppmoney.com>
2014-12-23 21:45:00 +0800

    表

---
+++ database/删除表.sql
@@ -0,0 +1,10 @@
+USE waimai
+GO
+DROP TABLE [Order]
+DROP TABLE FoodMenu
+DROP TABLE FoodMenuCategory
+DROP TABLE Restaurant
+DROP TABLE ExpendLog
+DROP TABLE Recharge
+DROP TABLE [User]
+DROP TABLE Config
